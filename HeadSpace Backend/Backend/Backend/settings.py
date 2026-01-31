5dd3ec00706d892f7441903d5729f9c811e41d32
wahumuchemi649 <lydiamuchemi260@gmail.com>
2026-01-31 18:00:15 +0300

    secret key

--- HeadSpace Backend/Backend/Backend/settings.py
+++ HeadSpace Backend/Backend/Backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8sb08c!%s49*yd^$#of68hej0qtilw$0cks)!&7%!--c0u-)93'
+SECRET_KEY = os.environ['SECRET_KEY'] 
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'localhost']
 
 
 # Application definition
