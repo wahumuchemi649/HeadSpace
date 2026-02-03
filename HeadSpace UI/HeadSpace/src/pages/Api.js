432e81bfd36ad22cb8b6c9cd00235bb0a9193db0
wahumuchemi649 <lydiamuchemi260@gmail.com>
2026-02-03 10:35:41 +0300

    Fixing vite

--- HeadSpace UI/HeadSpace/src/pages/Api.js
+++ HeadSpace UI/HeadSpace/src/pages/Api.js
@@ -1,3 +1,2 @@
-// Api.js
 export const Api_Base = import.meta.env.VITE_API_BASE;
-export const WS_BASE = Api_Base.replace(/^http/, "ws");
+export const WS_BASE = Api_Base.replace(/^http(s)?/, "ws$1");
