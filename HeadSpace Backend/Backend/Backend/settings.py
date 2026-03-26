b944e64f93bddcc2f15d7551e9f50abf81fc5b6a
wahumuchemi649 <lydiamuchemi260@gmail.com>
2026-03-27 00:33:19 +0300

    version 1.0 complete

--- HeadSpace Backend/Backend/Backend/settings.py
+++ HeadSpace Backend/Backend/Backend/settings.py
@@ -26,9 +26,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'chelsfavor@gmail.com'
-EMAIL_HOST_PASSWORD = 'bncb qtkm ieei gyzo' # ⚠️ Use environment variable!
-DEFAULT_FROM_EMAIL = 'chelsfavor@gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
